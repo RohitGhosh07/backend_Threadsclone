d7a0ed7a3e15ed72fc9cd8ec7a4c703c1b4fed96
ionicthor <ioniccompound6@gmail.com>
2023-09-17 10:48:47 +0530

    updates02

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
+const connectDB = async (MONGO_URI = "mongodb+srv://ioniccompound6:ioniccompound6@cluster0.41hsbgk.mongodb.net/threads?retryWrites=true&w=majority") => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI, {
+		// const conn = await mongoose.connect(process.env.MONGO_URI, {
+		const conn = await mongoose.connect(MONGO_URI, {
 			// To avoid warnings in the console
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
